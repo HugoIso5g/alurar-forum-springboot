be6f51da21084162d09eb544bf51b462e725282a
Hugo <hugopessanha22@gmail.com>
2022-07-11 16:36:04 -0300

    first commit

---
+++ src/main/java/br/com/alura/forum/repository/TopicoRepository.java
@@ -0,0 +1,18 @@
+package br.com.alura.forum.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.alura.forum.modelo.Topico;
+
+public interface TopicoRepository extends JpaRepository<Topico, Long> {
+
+	Page<Topico> findByCursoNome(String nomeCurso, Pageable paginacao);
+	
+	//@Query("SELECT t FROM Topico WHERE t.curso.nome = : nomeCurso")
+	//List<Topico> carregarPorNomeDoCurso(@Param("nomeCurso") String nomeCurso);
+
+}
